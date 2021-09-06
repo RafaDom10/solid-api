79b5ba94da5e9601d90f80acbb8fce145513b231
RafaDom10 <r-domingues@outlook.com>
2021-09-06 16:47:17 -0300

    done

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,8 @@
 import { Router } from 'express';
+import { createUserController } from './useCases/CreateUser';
 
 const router = Router();
+
+router.post('/users', (request, response) => createUserController.handle(request, response));
 
 export { router };
